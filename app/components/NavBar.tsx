05c29c2e4aa2af139fc931a01544ec5b01ea8f58
GabenNguyen <nbhapl128844@gmail.com>
2025-11-11 10:19:57 +1030

    Fix component appearance.

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -12,17 +12,17 @@ import { ModeToggle } from "./ModeToggle";
 function NavBar() {
     return (
         <nav className="bg-blue-200 dark:bg-black dark:text-white shadow-md">
-            <div className="max-w-5xl mx-auto flex flex-col items-center" >
+            <div className="max-w-5xl mx-auto flex items-center justify-between p-4" >
                 <Link href="/" className="text-5xl font-bold mt-3">
                     Quizzify
                 </Link>
 
-                <NavigationMenu>
-                    <NavigationMenuList className="flex justify-center space-x-6 mt-4 mb-2">
-                        {["Home", "Quiz", "Results"].map((item) => (
+                <NavigationMenu className="flex-1 -m-px flex justify-center">
+                    <NavigationMenuList className="flex items-center justify-center space-x-3 mt-4 mb-2">
+                        {["About", "Quiz", "Results"].map((item) => (
                             <NavigationMenuItem key={item}>
                                 <NavigationMenuLink asChild>
-                                    <Link href={item === "Home" ? `/`: `/${item.toLowerCase()}` } 
+                                    <Link href={item === "About" ? `/`: `/${item.toLowerCase()}` } 
                                     className="!text-xl hover:underline"
                                     >
                                         {item}
