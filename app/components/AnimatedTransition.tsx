fc87db83f5e40f0b2bdfda747c20675f3be6d361
GabenNguyen <nbhapl128844@gmail.com>
2025-11-11 13:17:40 +1030

    Add comments.

--- app/components/AnimatedTransition.tsx
+++ app/components/AnimatedTransition.tsx
@@ -5,13 +5,13 @@ import { AnimatePresence, motion } from 'framer-motion'
 import { usePathname } from 'next/navigation'
 
 function AnimatedTransition( {children}:{ children: React.ReactNode } ){
-    const pathName = usePathname()
+    const pathName = usePathname() // retrieves the current pathname from the URL
 
     return (
         <AnimatePresence mode='wait' initial={true}>
             <motion.div
                 key={pathName}
-                initial={{ opacity: 0, scale: 0.98 }}
+                initial={{ opacity: 0, scale: 0.98 }} // Contents appear
                 animate={{ opacity: 1, scale: 1 }}
                 transition={{ duration: 0.35, ease: "easeIn",  delay: 0.05}}
             >
