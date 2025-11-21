c2420717b4bc956c2cecd82be34cf522685fa038
GabenNguyen <nbhapl128844@gmail.com>
2025-11-21 11:49:48 +1030

    Modify the countdown logic so that when there is only 10 seconds left, the text turns red with light red background.

--- app/components/CountDown.tsx
+++ app/components/CountDown.tsx
@@ -31,7 +31,7 @@ function CountDown( { difficulty, onTimeUp }: CountDownTime ) {
     const seconds = counter % 60;
 
     return (
-        <div className={`text-center font-sans text-3xl ${minutes <= 5 && seconds <= 10 ? "text-white animate-pulse bg-red-600 rounded-lg" : ""}`}>
+        <div className={`text-center font-sans text-3xl ${minutes <= 0 && seconds <= 10 ? "text-red-600 animate-pulse bg-red-300 font-bold rounded-lg" : ""}`}>
             <span>{minutes.toString().padStart(2, "0")}:</span>
             <span>{seconds.toString().padStart(2, "0")}</span>
         </div>
