c9c951a7bb7ccb9a73592c6526a667d406fdb001
Ba Hoa NGUYEN <nbhapl128844@gmail.com>
2025-11-25 12:40:48 +1030

    Finish QuizSetup component.

--- app/components/QuizSetup.tsx
+++ app/components/QuizSetup.tsx
@@ -1,9 +1,8 @@
-import Question from '@/type/question';
+import sampleQuestions from '@/utils/sample_questions';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Spinner } from "@/components/ui/spinner";
 import { Label } from "@/components/ui/label";
-import { toast } from "sonner"
 import {
   Card,
   CardContent,
@@ -34,8 +33,75 @@ interface QuizSetupProps {
     isLoading: boolean
 }
 
-function QuizSetup( {} ) {
+function QuizSetup({ 
+  topic, 
+  difficulty, 
+  numberOfQuestions, 
+  setTopic, 
+  setDifficulty, 
+  setNumberOfQuestions, 
+  handleStartQuiz, 
+  isLoading 
+}: QuizSetupProps ) {
+  return (
+    <Card className="w-full max-w-3xl shadow-2xl border border-gray-300 dark:border-gray-700 rounded-2xl p-6">
+            <CardHeader className="text-center space-y-3">
+              <CardTitle className="text-4xl font-extrabold tracking-tight">
+                <span className="text-blue-600">What are you up to?</span> 
+              </CardTitle>
+              <CardDescription className="text-lg text-gray-600 dark:text-gray-400">
+                Select a topic, choose the difficulty and the number of questions then you're all good to go
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-6 text-center text-lg">
+            <Label htmlFor='topic' className='flex items-center gap-2 text-gray-700 dark:text-gray-300 font-semibold text-lg mb-2'>Topic</Label>
+            <Input
+                className="shadow-lg"
+                type="text"
+                placeholder="E.g. Art, History, Programming Languages."
+                value={topic}
+                onChange={(e) => setTopic(e.target.value)}
+            />
+            <Label htmlFor='difficulty' className='flex items-center gap-2 text-gray-700 dark:text-gray-300 font-semibold text-lg mb-2'>Difficulty</Label>
+            <Select value={difficulty} onValueChange={(difficulty) => setDifficulty(difficulty)}>
+                <SelectTrigger className="w-full shadow-lg cursor-pointer">
+                    <SelectValue placeholder="Choose your difficulty"/>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Difficulty</SelectLabel>
+                        <SelectItem value="easy" className='cursor-pointer hover:font-bold hover:text-lg'>Easy</SelectItem>
+                        <SelectItem value="medium" className='cursor-pointer hover:font-bold hover:text-lg'>Medium</SelectItem>
+                        <SelectItem value="hard" className='cursor-pointer hover:font-bold hover:text-lg'>Hard</SelectItem>
+                        <SelectItem value="asian" className='cursor-pointer hover:font-bold hover:text-lg'>Asian</SelectItem>
+                    </SelectGroup>
+                </SelectContent>
+            </Select>
 
+            <Label htmlFor='numOfQuestions' className='flex items-center gap-2 text-gray-700 dark:text-gray-300 font-semibold text-lg mb-2'>Number of Questions</Label>
+            <Input 
+                className="shadow-lg"
+                type='number'
+                min={`1`}
+                max={sampleQuestions.length}
+                placeholder="Choose the number of questions"
+                value={numberOfQuestions}
+                onChange={(e) => setNumberOfQuestions(e.target.value)}
+            />
+            <CardFooter className="flex justify-center">
+              <Button 
+              className="cursor-pointer active:scale-90 transition-all" 
+              type='submit' 
+              onClick={handleStartQuiz}
+              disabled={isLoading}
+              >
+                {isLoading ? <> Generating Quiz<Spinner /> </> : "Generate Quiz"}
+              </Button>
+            </CardFooter>
+          </CardContent>
+        
+        </Card>
+  )
 }
 
 export default QuizSetup;
