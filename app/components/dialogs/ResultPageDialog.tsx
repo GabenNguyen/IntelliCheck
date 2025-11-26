14eed2fd2974313bcb3e8a203f11453747a6c80c
GabenNguyen <nbhapl128844@gmail.com>
2025-11-26 21:28:22 +1030

    Add interface and return statement.

--- app/components/dialogs/ResultPageDialog.tsx
+++ app/components/dialogs/ResultPageDialog.tsx
@@ -1,3 +1,4 @@
+import React, { SetStateAction } from "react"
 import {
   AlertDialog,
   AlertDialogContent,
@@ -7,19 +8,26 @@ import {
 
 import { Spinner } from "@/components/ui/spinner";
 
-function ResultPageDialog( { openCondition, setOpenCondition }: any ) {
-    <AlertDialog open={openCondition} onOpenChange={setOpenCondition}>
-        <AlertDialogContent>
-            <AlertDialogHeader>
-            <AlertDialogTitle>
-                <div className="flex items-center justify-between">
-                    <span className='text-xl'>Result being calculated</span>
-                    <Spinner className='size-8'/>
-                </div>
-            </AlertDialogTitle>
-            </AlertDialogHeader>
-        </AlertDialogContent>
-    </AlertDialog>
+interface Props {
+    open: boolean,
+    setOpen: React.Dispatch<SetStateAction<boolean>>
+}
+
+function ResultPageDialog( { open, setOpen }: Props ) {
+    return (
+        <AlertDialog open={open} onOpenChange={setOpen}>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                <AlertDialogTitle>
+                    <div className="flex items-center justify-between">
+                        <span className='text-xl'>Result being calculated</span>
+                        <Spinner className='size-8'/>
+                    </div>
+                </AlertDialogTitle>
+                </AlertDialogHeader>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
 }
 
 export default ResultPageDialog
