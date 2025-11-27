807108bf576ca9fab3e1e203ec53d0d46c59fd99
GabenNguyen <nbhapl128844@gmail.com>
2025-11-27 13:31:15 +1030

    Delete unnecessary import.

--- app/results/page.tsx
+++ app/results/page.tsx
@@ -1,10 +1,8 @@
 "use client";
-import React from 'react'
 import { useState, useEffect} from 'react';
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import Answer from '@/type/answer';
-import { toast } from "sonner"
 import {
   Card,
   CardContent,
