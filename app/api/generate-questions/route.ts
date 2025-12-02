a41c72da09fc5e55a5912257d94a3b5bf586053b
GabenNguyen <nbhapl128844@gmail.com>
2025-12-02 11:33:48 +1030

    Add difficulty rules.

--- app/api/generate-questions/route.ts
+++ app/api/generate-questions/route.ts
@@ -1,4 +1,6 @@
 import { GoogleGenAI } from "@google/genai";
+import { NextResponse } from "next/server";
+import DifficultyRules from "@/utils/difficulty_rules";
 
 const GEMINI_AI_KEY = process.env.GEMINI_API_KEY;
 
@@ -6,12 +8,13 @@ export async function POST(req: Request) {
     try {
         // wait for the user input
         const { topic, difficulty, numOfQuestions} = await req.json();
+        const difficultyRules = DifficultyRules(difficulty)
         
         const ai = new GoogleGenAI({apiKey: GEMINI_AI_KEY});
         
         const prompt = `
             Generate ${numOfQuestions} questions about ${topic}
-            Difficulty: ${difficulty}
+            Difficulty: ${difficultyRules}
             Strictly format the response in JSON like this:
 
             [
@@ -50,15 +53,11 @@ export async function POST(req: Request) {
         const outputQuestion = JSON.parse(cleanText);
 
 
-        return new Response(
-            JSON.stringify({ outputQuestion }), { status: 200}
-        );
+       return NextResponse.json({ outputQuestion }, {status: 200})
 
     } catch (error) {
         console.error(`AI error: ${error}`)
         
-        return new Response(
-            JSON.stringify({ error: "AI Generation Failed"}), { status: 500 }
-        )
+        return NextResponse.json( { error: "AI Generation Failed!" }, { status: 500 } )
     }
 }
