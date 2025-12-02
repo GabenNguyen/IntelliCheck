801e9b6f13103c6b08e00da3aed06a76b4433d62
Ba Hoa NGUYEN <nbhapl128844@gmail.com>
2025-12-02 10:53:19 +1030

    Wrap all quiz input boxes such as topic, difficulty selection and number of questions into a form so that user can hit enter to submit.

--- app/components/quiz_related/QuizSetup.tsx
+++ app/components/quiz_related/QuizSetup.tsx
@@ -1,8 +1,8 @@
-import sampleQuestions from '@/utils/sample_questions';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { Spinner } from "@/components/ui/spinner";
 import { Label } from "@/components/ui/label";
+
 import {
   Card,
   CardContent,
@@ -44,7 +44,7 @@ function QuizSetup({
   isLoading 
 }: Props ) {
   return (
-    <Card className="w-full max-w-3xl shadow-2xl border border-gray-400 dark:border-gray-700 rounded-2xl p-6">
+        <Card className="w-full max-w-3xl shadow-2xl border border-gray-400 dark:border-gray-700 rounded-2xl p-6">
             <CardHeader className="text-center space-y-3">
               <CardTitle className="text-4xl font-extrabold tracking-tight">
                 <span className="text-blue-600">What are you up to?</span> 
@@ -53,54 +53,58 @@ function QuizSetup({
                 Select a topic, choose the difficulty and the number of questions then you're all good to go
               </CardDescription>
             </CardHeader>
-            <CardContent className="space-y-6 text-center text-lg">
-            <Label htmlFor='topic' className='flex items-center gap-2 text-gray-700 dark:text-gray-300 font-semibold text-lg mb-1'>Topic</Label>
-            <Input
-                className="shadow-lg border-black"
-                type="text"
-                placeholder="E.g. Art, History, Programming Languages."
-                value={topic}
-                onChange={(e) => setTopic(e.target.value)}
-            />
-            <Label htmlFor='difficulty' className='flex items-center gap-2 text-gray-700 dark:text-gray-300 font-semibold text-lg mb-1'>Difficulty</Label>
-            <Select value={difficulty} onValueChange={(difficulty) => setDifficulty(difficulty)}>
-                <SelectTrigger className="w-full shadow-lg cursor-pointer border-black">
-                    <SelectValue placeholder="Choose your difficulty"/>
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Difficulty</SelectLabel>
-                        <SelectItem value="easy" className='cursor-pointer hover:font-bold hover:text-lg'>Easy</SelectItem>
-                        <SelectItem value="medium" className='cursor-pointer hover:font-bold hover:text-lg'>Medium</SelectItem>
-                        <SelectItem value="hard" className='cursor-pointer hover:font-bold hover:text-lg'>Hard</SelectItem>
-                        <SelectItem value="asian" className='cursor-pointer hover:font-bold hover:text-lg'>Asian</SelectItem>
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
+            <form onSubmit={handleStartQuiz}>
+              <CardContent className="space-y-6 text-center text-lg">
+                <Label htmlFor='topic' className='flex items-center gap-2 text-gray-700 dark:text-gray-300 font-semibold text-lg mb-1'>Topic</Label>
+                <Input
+                    id='topic'
+                    className="shadow-lg border-black"
+                    type="text"
+                    placeholder="E.g. Art, History, Programming Languages."
+                    value={topic}
+                    onChange={(e) => setTopic(e.target.value)}
+                />
+                <Label htmlFor='difficulty' className='flex items-center gap-2 text-gray-700 dark:text-gray-300 font-semibold text-lg mb-1'>Difficulty</Label>
+                <Select value={difficulty} onValueChange={(difficulty) => setDifficulty(difficulty)}>
+                    <SelectTrigger id='difficulty' className="w-full shadow-lg cursor-pointer border-black">
+                        <SelectValue placeholder="Choose your difficulty"/>
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Difficulty</SelectLabel>
+                            <SelectItem value="easy" className='cursor-pointer hover:font-bold hover:text-lg'>Easy</SelectItem>
+                            <SelectItem value="medium" className='cursor-pointer hover:font-bold hover:text-lg'>Medium</SelectItem>
+                            <SelectItem value="hard" className='cursor-pointer hover:font-bold hover:text-lg'>Hard</SelectItem>
+                            <SelectItem value="asian" className='cursor-pointer hover:font-bold hover:text-lg'>Asian</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
 
-            <Label htmlFor='numOfQuestions' className='flex items-center gap-2 text-gray-700 dark:text-gray-300 font-semibold text-lg mb-1'>Number of Questions</Label>
-            <Input 
-                className="shadow-lg border-black"
-                type='number'
-                min={`1`}
-                max={sampleQuestions.length}
-                placeholder="Choose the number of questions"
-                value={numberOfQuestions}
-                onChange={(e) => setNumberOfQuestions(e.target.value)}
-            />
-            <CardFooter className="flex justify-center">
-              <Button 
-              className="cursor-pointer active:scale-90 transition-all" 
-              type='submit' 
-              onClick={handleStartQuiz}
-              disabled={isLoading}
-              >
-                {isLoading ? <> Generating Quiz<Spinner /> </> : "Generate Quiz"}
-              </Button>
-            </CardFooter>
-          </CardContent>
-        
-        </Card>
+                <Label htmlFor='numOfQuestions' className='flex items-center gap-2 text-gray-700 dark:text-gray-300 font-semibold text-lg mb-1'>Number of Questions</Label>
+                <Input 
+                    id='numOfQuestions'
+                    className="shadow-lg border-black"
+                    type='number'
+                    min={1}
+                    max={50}
+                    placeholder="Choose the number of questions"
+                    value={numberOfQuestions}
+                    onChange={(e) => setNumberOfQuestions(e.target.value)}
+                    required
+                />
+                <CardFooter className="flex justify-center">
+                  <Button 
+                  className="cursor-pointer active:scale-90 transition-all" 
+                  type='submit' 
+                  onClick={handleStartQuiz}
+                  disabled={isLoading}
+                  >
+                    {isLoading ? <> Generating Quiz<Spinner /> </> : "Generate Quiz"}
+                  </Button>
+                </CardFooter>
+            </CardContent>
+        </form>
+      </Card>
   )
 }
 
