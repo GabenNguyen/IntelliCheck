9d927754c29c48c3b1bc8b7d50cf157aed89b5fc
Ba Hoa NGUYEN <nbhapl128844@gmail.com>
2025-12-04 17:26:27 +1030

    Finish the sign-in page.

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs"
 export default function SignInPage () {
   return (
     <div className="flex justify-center items-center min-h-[80vh] py-10">
-        <SignIn />
+        <SignIn path="/sign-in" routing="path"/>
     </div>
   )
 }
