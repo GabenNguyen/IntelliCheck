67aa5812ecceac7711fe023412a616ebcb766bf1
Ba Hoa NGUYEN <nbhapl128844@gmail.com>
2025-12-04 17:30:23 +1030

    Add signUpUrl in the SignIn to redirect to the sign-up page.

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs"
 export default function SignInPage () {
   return (
     <div className="flex justify-center items-center min-h-[80vh] py-10">
-        <SignIn path="/sign-in" routing="path"/>
+        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up"/>
     </div>
   )
 }
