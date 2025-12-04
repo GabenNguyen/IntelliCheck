38fb43c586a48863d1712f4f8dc06ffaef6b6830
Ba Hoa NGUYEN <nbhapl128844@gmail.com>
2025-12-04 17:27:31 +1030

    Finish sign-up page.

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function SignUpPage() {
     return (
         <div className="flex justify-center items-center min-h-[80vh] py-10">
-            <SignUp />
+            <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
         </div>
     )
 }
