ecc7ad5953610003d7a609a20fe2b9191ec5cd2d
Ba Hoa NGUYEN <nbhapl128844@gmail.com>
2025-12-11 16:07:16 +1030

    Save user into the database.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,13 +1,34 @@
 import Dashboard from "../components/DashBoard";
 import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import prisma from "@/lib/db";
+
 
 export default async function DashBoardPage() {
-    const user = await currentUser();
-    
-    const userName = user?.username || user?.firstName || "";
+    try {
+        const clerkUser = await currentUser();
 
-    return (
-        <Dashboard userName={userName} />
-    )
+        if(!clerkUser) {
+            redirect("/sign-in")
+        }
+
+        const user = await prisma.user.upsert({
+            where: {id: clerkUser.id},
+            update: {},
+            create: {
+                id: clerkUser.id,
+                email: clerkUser.emailAddresses[0]?.emailAddress || "",
+                name: `${clerkUser.firstName || ""} ${clerkUser.lastName || ""}`.trim() || "User"
+            },
+        })
+
+
+        return (
+            <Dashboard userName={user.name} />
+        )
+    } catch (error) {
+        console.error("Error loading dashboard page!", error)
+    }
+    
 
 }
