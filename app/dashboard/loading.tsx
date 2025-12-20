ffcab007b4edd2505e13bc64ff87cdd2d6f47676
Ba Hoa NGUYEN <nbhapl128844@gmail.com>
2025-12-20 10:59:37 +1030

    Change the loading animation and text to white in dark theme and vice versa.

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -3,8 +3,10 @@ export default function DashboardReloading() {
     <div className="flex min-h-screen items-center justify-center">
       <div className="flex flex-col items-center gap-4">
         {/* spinner */}
-        <div className="h-12 w-12 animate-spin animate-fade-in duration-300 rounded-full border-4 border-blue-600 border-t-transparent" />
-        <p className="text-gray-600 font-medium">Loading dashboard data...</p>
+        <div className="h-12 w-12 animate-spin animate-fade-in duration-300 rounded-full border-4 border-black dark:border-white border-t-transparent" />
+        <p className="dark:text-white text-black font-medium">
+          Loading dashboard data...
+        </p>
       </div>
     </div>
   );
