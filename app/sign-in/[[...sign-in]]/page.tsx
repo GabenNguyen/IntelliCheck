1c801e1eaeb6d42e1a4112644597082da685b784
GabenNguyen <nbhapl128844@gmail.com>
2025-12-22 11:47:56 +1030

    Add forceRedirectUrl to redirect to the dashboard page after sign-in.

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,14 @@
-import { SignIn } from "@clerk/nextjs"
+import { SignIn } from "@clerk/nextjs";
 
-export default function SignInPage () {
+export default function SignInPage() {
   return (
     <div className="flex justify-center items-center min-h-[80vh] py-10">
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up"/>
+      <SignIn
+        path="/sign-in"
+        routing="path"
+        signUpUrl="/sign-up"
+        forceRedirectUrl="/dashboard"
+      />
     </div>
-  )
+  );
 }
-
